c498ab6a9daf5ac2ca45db04f2a3716361f6e7c6
tyler rogel <trogel138@gmail.com>
2022-07-14 09:14:12 -0600

    fixed

--- persist/connect.js
+++ persist/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const db = mongoose.connection;
 
 async function connect(user, pass) {
-  const connectString = `mongodb+srv://trogel138:mongodb42@cluster0.swyngnk.mongodb.net/?retryWrites=true&w=majority`;
+  const connectString = `mongodb+srv://trogel138:${pass}@cluster0.swyngnk.mongodb.net/?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(connectString, {
       useNewUrlParser: true,
